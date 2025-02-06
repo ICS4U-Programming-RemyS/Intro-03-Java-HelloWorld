03cd3e7570819dc3aab9b9f1bf1758dfbfcb2f83
Remy Skelton <remy.skelton@stu.ocsb.ca>
2025-02-06 01:08:59 +0000

    Done Hello World

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+* This program will display hello to someone and an additional statement.
+*
+* @author  Remy Skelton
+* @version 1.0
+* @since   2025-February-5
+*/
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Santiago");
+        System.out.println("How is the new semester?");
+    }
+}
